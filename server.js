d13eb861f72aed480ff09dcff308dd8a9de8dde2
SUDO-j182 <jai.onions97@gmail.com>
2025-04-23 21:12:24 +0100

    Finalized implementation of CRUD API

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ const PORT = process.env.PORT || 3000;//sets port to 3000 or the env variable PO
 app.get('/api/tasks', (req,res) => {
   res.json(tasks);//sends back the task array 
 });
+
+                                         //GET TASK BY ID
+app.get('/api/tasks/:id', (req, res) => {
+  const taskId = parseInt(req.params.id, 10);//converts id from a string to a number
+  const task = tasks.find(t => t.id === taskId);
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found.'});//display error if task is not found
+    }
+    res.json(task);//gets the located task 
+});
                                      //POST API TASKS
 app.post('/api/tasks', (req,res) => {
   const { title, description, datetime, status} = req.body;
@@ -28,6 +38,7 @@ app.post('/api/tasks', (req,res) => {
   tasks.push(newTask);
   res.status(201).json(newTask);//respond with the new task object
 });
+
                           //PORT LISTENER
   app.listen(PORT, () => {//runs function when server starts
     console.log(`Server is running on http://localhost:${PORT}`);//server URL
